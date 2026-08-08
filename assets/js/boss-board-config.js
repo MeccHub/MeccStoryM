a61446bb67d4cba49a19c77fb681496c77bdf924
MeccHub <mecchub@users.noreply.github.com>
2026-08-08 11:21:23 -0400

    Configure live boss board Worker

--- assets/js/boss-board-config.js
+++ assets/js/boss-board-config.js
@@ -1,3 +1,1 @@
-// Set this to the deployed Cloudflare Worker board endpoint, for example:
-// window.MSM_BOSS_BOARD_API = "https://meccstorym-boss-board.your-subdomain.workers.dev/current";
-window.MSM_BOSS_BOARD_API = window.MSM_BOSS_BOARD_API || "";
+window.MSM_BOSS_BOARD_API = window.MSM_BOSS_BOARD_API || "https://meccstorym-boss-board.boss-board.workers.dev/current";
